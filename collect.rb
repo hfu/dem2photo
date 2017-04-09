c9784bad37919411ba507dce9b6e3c125bca898a
hfu <hfu@github.com>
2017-04-09 15:07:24 +0900

    update

--- collect.rb
+++ collect.rb
@@ -1,13 +1,12 @@
 require 'find'
 require 'fileutils'
 
-Find.find('/Volumes/Extreme 900/dem_png/10') {|a_path|
+Find.find('/tmp/source/dem_png/10') {|a_path|
   next unless /(\d*)\/(\d*)\/(\d*)\.png$/.match a_path
   (z, x, y) = [$1, $2, $3].map{|v| v.to_i}
-  b_path = "/Volumes/Extreme 900/seamlessphoto/#{z}/#{x}/#{y}.jpg"
+  b_path = "/tmp/source/seamlessphoto/#{z}/#{x}/#{y}.jpg"
   next unless File.exist?(b_path)
   %w{test train val}.each{|t|
-    FileUtils.cp(a_path, "dem_png/#{t}/#{z}-#{x}-#{y}.png")
-    FileUtils.cp(b_path, "seamlessphoto/#{t}/#{z}-#{x}-#{y}.jpg")
+    print "convert #{a_path} #{b_path} +append #{t}/#{z}-#{x}-#{y}.png\n"
   }
 }
